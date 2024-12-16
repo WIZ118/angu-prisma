b500f5560975942920a3a341b95b1e33582ca61e
WIZ118 <Iniackie118@gmail.com>
2024-12-16 19:30:56 +0800

    routing latest

--- real-estate-app/src/app/app.routes.ts
+++ real-estate-app/src/app/app.routes.ts
@@ -2,22 +2,24 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin/admin.component';
 import { AddhouseComponent } from './admin/addhouse/addhouse.component';
 import { UsersComponent } from './admin/users/users.component';
 import { ManageHousesComponent } from './admin/manage-houses/manage-houses.component';
+import { EditHouseComponent } from './admin/edit-house/edit-house.component';
 import { AuthGuard } from './auth.guard';
+import { PropertyDetailsComponent } from './property-details/property-details.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent },
   {
-    path: 'dashboard',
-    component: DashboardComponent,
+    path: 'property/:id',
+    component: PropertyDetailsComponent,
     canActivate: [AuthGuard],
-  },
+  }, // Add AuthGuard here
   {
     path: 'admin',
     component: AdminComponent,
@@ -27,7 +29,8 @@ export const routes: Routes = [
       { path: 'add-house', component: AddhouseComponent },
       { path: 'users', component: UsersComponent },
       { path: 'manage-houses', component: ManageHousesComponent },
+      { path: 'edit-house/:id', component: EditHouseComponent },
     ],
   },
-  { path: '**', redirectTo: 'login' },
+  { path: '**', redirectTo: 'home' },
 ];
